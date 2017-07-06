1e6a7456744290848a599dbfab9de018684be084
Matias <matias87lp@gmail.com>
2017-07-06 22:24:30 +0100

    primer version funcionando casi completa (falta comprobar marcha atras y motor estribor)

--- arduino/IR/IR-controlled/uss-hornet/uss-hornet.ino
+++ arduino/IR/IR-controlled/uss-hornet/uss-hornet.ino
@@ -6,7 +6,7 @@
  */
 
 #include "IRremote.h"
-#include "Servo.h"
+#include <Servo.h>
 
 
 /* PINs Declaration */
@@ -14,8 +14,8 @@ int PIN_IR_RECEIVER = 13;
 int PIN_LED_TORRE = 10;
 int PIN_LED_CASCO  = 9; 
 int PIN_SERVO_TIMON = 8;
-int PIN_MTE_ENABLED = 7;
-int PIN_MTB_ENABLED = 6;
+//int PIN_MTE_ENABLED = 7;
+//int PIN_MTB_ENABLED = 6;
 int PIN_MTE_DIR_A = 5;
 int PIN_MTE_DIR_B = 4;
 int PIN_MTB_DIR_A = 3;
@@ -33,10 +33,10 @@ decode_results irCommand;      // create instance of 'decode_results'
 /* Ship intants */
 int TIMON_MIN = 30;
 int TIMON_MAX = 150;
-int MOTOR_MAX = 255;
+int MOTOR_MAX = 127;
 int MOTOR_MIN = -64;
 int MOTOR_STEP = 32;
-int TIMON_STEP = 30;
+int TIMON_STEP = 20;
 int TIMON_STOP = 90;
 
 /* Ship Status */
@@ -45,144 +45,161 @@ int velMotorBabor = 0;
 int velMotorEstribor = 0;
 int luzCasco = LOW;
 int luzTorre = LOW;
-int posTimonDest = 0;
+int posTimonDest = TIMON_STOP;
 int velMotorBaborDest = 0;
 int velMotorEstriborDest = 0;
 
-/* calcula el proximo estado de un led*/
-int switchLed(int previousValue) {
-    return HIGH - previousValue;
-}
+/* Apply change */
+bool cambiarTimon = true;
+bool cambiarMotorB = true;
+bool cambiarMotorE = true;
+bool cambiarLuzCasco = true;
+bool cambiarLuzTorre = true;
+
 
 void manageIRAction() {
-  irrecv.resume(); // receive the next value
   if (irrecv.decode(&irCommand)) { // have we received an IR signal?
     switch(irCommand.value) {
-    case 0xFFA25D: Serial.println("POWER"); break;
-    case 0xFFE21D: //Serial.println("FUNC/STOP");
-      // Pongo en 0 el status de movimiento del barco 
-      posTimon = TIMON_STOP;
-      velMotorBabor = 0;
-      velMotorEstribor = 0;
-      break;
-    case 0xFF629D: //Serial.println("VOL+"); 
+    //case 0xFFA25D: Serial.println("POWER"); break;
+    //case 0xFFE21D: Serial.println("FUNC/STOP"); break;
+    case 0xFF629D: Serial.println("VOL+"); 
       //Subo la velocidad general
       velMotorBaborDest += MOTOR_STEP;
       velMotorEstriborDest += MOTOR_STEP;
       break;
-    case 0xFF22DD: //Serial.println("FAST BACK");    
+    case 0xFF22DD: Serial.println("FAST BACK");    
       // Muevo el timon para Babor
       posTimonDest -= TIMON_STEP;
       break;
-    case 0xFF02FD: //Serial.println("PAUSE");    
+    case 0xFF02FD: Serial.println("PAUSE");    
       // Pongo en 0 el status de movimiento del barco con los destions 
       posTimonDest = TIMON_STOP;
       velMotorBaborDest = 0;
       velMotorEstriborDest = 0;      
       break;
-    case 0xFFC23D: //Serial.println("FAST FORWARD");   
+    case 0xFFC23D: Serial.println("FAST FORWARD");   
       // Muevo el timon para Estribor
       posTimonDest += TIMON_STEP;
       break;
-    case 0xFFE01F: //Serial.println("DOWN");    
+    case 0xFFE01F: Serial.println("DOWN");    
       // incremento motor babor solamente
       velMotorBaborDest += MOTOR_STEP;
       break;
-    case 0xFFA857: //Serial.println("VOL-");    
+    case 0xFFA857: Serial.println("VOL-");    
       //Bajo la velocidad general
       velMotorBaborDest -= MOTOR_STEP;
       velMotorEstriborDest -= MOTOR_STEP;
       break;
-    case 0xFF906F: //Serial.println("UP");    
+    case 0xFF906F: Serial.println("UP");    
       // Incremento estribor solamente;
       velMotorEstriborDest += MOTOR_STEP;
       break;
-    case 0xFF9867: Serial.println("EQ");    break;
-    case 0xFFB04F: //Serial.println("ST/REPT");    
+    //case 0xFF9867: Serial.println("EQ");    break;
+    case 0xFFB04F: Serial.println("ST/REPT");    
       // Decremento estribor solamente
       velMotorEstriborDest -= MOTOR_STEP;
       break;
-    case 0xFF6897: //Serial.println("0");    
+    case 0xFF6897: Serial.println("0");    
       // Decremento motor babor solamente
       velMotorBaborDest -= MOTOR_STEP;
       break;
-    case 0xFF30CF: Serial.println("1");    break;
-    case 0xFF18E7: Serial.println("2");    break;
-    case 0xFF7A85: Serial.println("3");    break;
-    case 0xFF10EF: Serial.println("4");    break;
-    case 0xFF38C7: Serial.println("5");    break;
-    case 0xFF5AA5: Serial.println("6");    break;
-    case 0xFF42BD: Serial.println("7");    break;
-    case 0xFF4AB5: //Serial.println("8");    
+    //case 0xFF30CF: Serial.println("1");    break;
+    //case 0xFF18E7: Serial.println("2");    break;
+    //case 0xFF7A85: Serial.println("3");    break;
+    //case 0xFF10EF: Serial.println("4");    break;
+    //case 0xFF38C7: Serial.println("5");    break;
+    //case 0xFF5AA5: Serial.println("6");    break;
+    //case 0xFF42BD: Serial.println("7");    break;
+    case 0xFF4AB5: Serial.println("8");    
       // cambio luz de bajo cubierta
       luzCasco = switchLed(luzCasco);
       break;
-    case 0xFF52AD: //Serial.println("9");    
+    case 0xFF52AD: Serial.println("9");    
       // cambio luz de torre;
       luzTorre = switchLed(luzTorre);
       break;
-    case 0xFFFFFFFF: Serial.println(" REPEAT");break;  
+    //case 0xFFFFFFFF: Serial.println(" REPEAT");break;  
   
     default: 
       Serial.println(" other button   ");
   
     }// End Case
+    irrecv.resume(); // receive the next value
   }
 } //END translateIR
 
 int transitionVariable(int orig, int dest) {
   if (orig > dest) {
-    return orig--;
+    return orig - 1;
   } else if (orig < dest) {
-    return orig++;  
+    return orig + 1;  
   } else {
     return orig;  
   }
 }
 
-/* Move the transition values to the destinations */
-void transitionToDestinationVariables() {
-  posTimon = transitionVariable(posTimon, posTimonDest);
-  velMotorEstribor = transitionVariable(velMotorEstribor, velMotorEstriborDest);
-  velMotorBabor = transitionVariable(velMotorBabor, velMotorBaborDest);
+int switchLed(int actualValue) {
+  return HIGH - actualValue;  
 }
 
 
 /* Controla que los limites del timon y los motores no se pasen*/
 void controlLimites() {
- posTimon = min(max(posTimon, TIMON_MAX), TIMON_MIN);
- velMotorBabor = min(max(velMotorBabor, MOTOR_MAX), MOTOR_MIN);
- velMotorEstribor = min(max(velMotorEstribor, MOTOR_MAX), MOTOR_MIN);
+ posTimon = max(min(posTimon, TIMON_MAX), TIMON_MIN);
+ velMotorBabor = max(min(velMotorBabor, MOTOR_MAX), MOTOR_MIN);
+ velMotorEstribor = max(min(velMotorEstribor, MOTOR_MAX), MOTOR_MIN);
+ posTimonDest = max(min(posTimonDest, TIMON_MAX), TIMON_MIN);
+ velMotorBaborDest = max(min(velMotorBaborDest, MOTOR_MAX), MOTOR_MIN);
+ velMotorEstriborDest = max(min(velMotorEstriborDest, MOTOR_MAX), MOTOR_MIN);
 }
 
 /* Apply the motor speed to the specific motor's pins, TODO: Check the direction of the pins*/
-void applyVelMotor(int pinE, int pinA, int pinB, int vel) {
-  if (vel >= 0) {
+void applyVelMotor(int pinA, int pinB, int vel) {
     digitalWrite(pinB, LOW);
-    digitalWrite(pinA, HIGH);
-    analogWrite(pinE, vel);
-  } else {
     digitalWrite(pinA, LOW);
-    digitalWrite(pinB, HIGH);
-    analogWrite(pinE, -vel);
+ 
+  if (vel > 0) {
+    analogWrite(pinA, 127 + vel);
+  } else if (vel < 0) {
+    analogWrite(pinB, 127 - vel);
   }
+}
+
+void printTransition(char* control, int origen, int destino) {
+    Serial.print(control);
+    Serial.print(" - Dest: ");
+    Serial.print(destino);
+    Serial.print(", Actual: ");
+    Serial.println(origen);  
 }
 
 /* Apply the calculated state to the elements on the ship */
 void applyStateToShip() {
   controlLimites();
-  svTimon.write(posTimon);
-  applyVelMotor(PIN_MTE_ENABLED, PIN_MTE_DIR_A, PIN_MTE_DIR_B, velMotorEstribor);
-  applyVelMotor(PIN_MTB_ENABLED, PIN_MTB_DIR_A, PIN_MTB_DIR_B, velMotorBabor);
-  analogWrite(PIN_LED_TORRE, luzTorre);
-  analogWrite(PIN_LED_CASCO, luzCasco);
+  if (posTimon != posTimonDest) {
+    posTimon = transitionVariable(posTimon, posTimonDest);
+    printTransition("Timon", posTimon, posTimonDest);
+    svTimon.write(posTimon);
+  }
+  if (velMotorEstribor != velMotorEstriborDest) {
+    velMotorEstribor = transitionVariable(velMotorEstribor, velMotorEstriborDest);
+    printTransition("Estribor", velMotorEstribor, velMotorEstriborDest);
+    applyVelMotor(PIN_MTE_DIR_A, PIN_MTE_DIR_B, velMotorEstribor);
+  }
+  if (velMotorBabor != velMotorBaborDest) {
+    velMotorBabor = transitionVariable(velMotorBabor, velMotorBaborDest);
+    printTransition("Babor", velMotorBabor, velMotorBaborDest);
+    applyVelMotor(PIN_MTB_DIR_A, PIN_MTB_DIR_B, velMotorBabor);
+  }
+  digitalWrite(PIN_LED_TORRE, luzTorre);
+  digitalWrite(PIN_LED_CASCO, luzCasco);
 }
 
 void setup()   /*----( SETUP: RUNS ONCE )----*/
 {
   Serial.begin(9600);
   Serial.println("USS-Hornet IR-controlled Started"); 
-  irrecv.enableIRIn(); // Start the receiver
+  irrecv.enableIRIn(); // -Start the receiver
   svTimon.attach(PIN_SERVO_TIMON);
   applyStateToShip();
 
@@ -192,7 +209,6 @@ void setup()   /*----( SETUP: RUNS ONCE )----*/
 void loop()   /*----( LOOP: RUNS intANTLY )----*/
 {
   manageIRAction();
-  transitionToDestinationVariables();
   applyStateToShip();
 }/* --(end main loop )-- */
 
